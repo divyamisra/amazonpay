d3a5d9fb1faee59ba5f18b96a0b8479426f740d5
StuHeart <stuart.sims@heart.org>
2019-11-11 15:08:48 -0600

    HW Applepay form updates

--- heartwalk/applepay/js/donation-custom.js
+++ heartwalk/applepay/js/donation-custom.js
@@ -13,7 +13,7 @@
     });
 })(jQuery);
 
-jQuery(document).ready(function() {
+// jQuery(document).ready(function() {
 	jQuery('#from_url_js').val(document.referrer);
 	    
         var evid = jQuery.getQuerystring("FR_ID");
@@ -92,7 +92,7 @@ jQuery(document).ready(function() {
             });
         }
 
-    });
+    // });
 
 
 function donateApplePay() {
