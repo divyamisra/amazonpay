0fac96bf0166e660dab367d57aa679074a4a217b
Stuart Sims <stuart.sims@heart.org>
2020-06-25 16:00:39 -0500

    Remove duplicate functions
    * Consolidate event info display
    * Remove donateoffline call

--- ym-primary/js/donation-venmo.js
+++ ym-primary/js/donation-venmo.js
@@ -1,5 +1,5 @@
-    jQuery(document).ready(function() {
-        jQuery('#from_url_js').val(document.referrer);
+jQuery(document).ready(function() {
+	jQuery('#from_url_js').val(document.referrer);
 
 	var evid = jQuery.getQuerystring("FR_ID");
 	var apiURL = 'https://www2.heart.org/site/CRTeamraiserAPI?luminateExtend=1.7.1&method=getTeamraisersByInfo&name=%25%25%25&list_filter_column=frc.fr_id&list_filter_text='+evid+'&list_page_size=500&list_ascending=false&list_sort_column=event_date&api_key=wDB09SQODRpVIOvX&response_format=json&suppress_response_codes=true&v=1.0&ts=1536362358137';
@@ -140,7 +140,6 @@ function donateVenmo() {
 	var zip = jQuery('input[name="zip"]').val();
 	var venmouser = jQuery('input[name="venmo_user"]').val();
 	//var country = jQuery('select[name="country"]').val();
-	//var ref = data.donationResponse.donation.confirmation_code;
 
 	jQuery('.donation-loading').remove();
 	jQuery('.donate-now, .header-donate').hide();
@@ -181,21 +180,6 @@ function donateVenmo() {
 	ga('send', 'pageview', '/donateok.asp');
 }
 
-// function donateOffline() {
-// 	var params = jQuery('.donation-form').serialize();
-
-// 	jQuery.ajax({
-// 		method: "POST",
-// 		async: false,
-// 		cache: false,
-// 		dataType: "json",
-// 		url: "https://hearttools.heart.org/donate/convio-offline/addOfflineDonation-tr.php?" + params + "&callback=?",
-// 		success: function(data) {
-// 			//donateCallback.success(data.data);
-// 		}
-// 	});
-
-// }
 
 //copy donor fields to billing
 jQuery('[id^=donor_]').each(function() {
@@ -203,29 +187,3 @@ jQuery('[id^=donor_]').each(function() {
         jQuery("[id='" + jQuery(this).attr("id").replace("donor_", "billing_") + "']").val(jQuery(this).val());
     });
 });
-if (location.href.indexOf("donate_venmo") > 0) {
- 
-	var eid = jQuery('input[name=fr_id]').val();
-	var dtype = (jQuery('input[name=proxy_type_value]').val() == 20) ? "p" : ((jQuery('input[name=proxy_type_value]').val() == 21) ? "e" : "t");
-	var pid = (dtype == "p") ? jQuery('input[name=cons_id]').val() : "";
-	var tid = (dtype == "t") ? jQuery('input[name=team_id]').val() : "";
-    	var tr_info = "https://www2.heart.org/site/SPageNavigator/reus_donate_amazon_tr_info.html";
-    	if (jQuery('input[name=instance]').val() == "heartdev") {
-		tr_info = "https://secure3.convio.net/heartdev/site/SPageNavigator/reus_donate_amazon_tr_info.html";
-	}
-	jQuery.getJSON(tr_info+"?pgwrap=n&fr_id="+eid+"&team_id="+tid+"&cons_id="+pid+"&callback=?",function(data2){
-		//jQuery('.page-header h1').html(data2.event_title);
-		if (data2.team_name != "" && dtype == "t") {
-			jQuery('.donation-form-container').before('<div class="donation-detail"><strong>Donating to Team Name:</strong><br/><a href="'+jQuery('input[name=from_url]').val()+'">'+data2.team_name+'</a></div>');
-		}
-		if (data2.event_title != " " && dtype == "e") {
-			jQuery('.donation-form-container').before('<div class="donation-detail"><strong>Donating to Event:</strong><br/><a href="'+jQuery('input[name=from_url]').val()+'">'+data2.event_title+'</a></div>');
-		}
-		if (data2.part_name != " " && dtype == "p") {
-			jQuery('.donation-form-container').before('<div class="donation-detail"><strong>Donating to Student:</strong><br/><a href="'+jQuery('input[name=from_url]').val()+'">'+data2.part_name+'</a></div>');
-		}
-
-		jQuery('input[name=form_id]').val(data2.don_form_id);
-	});
-
-}
