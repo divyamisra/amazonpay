c5a7c39726470c5d2c10214dae3441511b8c873a
Stuart Sims <stuart.sims@heart.org>
2020-06-25 15:56:18 -0500

    Consolidate event info display

--- ym-primary/js/donation.js
+++ ym-primary/js/donation.js
@@ -37,7 +37,6 @@ jQuery('#cover_fee, .radio-level').on('click', function(){
 });
 
 
-
 // Post offline gift to Luminate
 function donateOffline() {
 	var params = jQuery('.donation-form').serialize();
@@ -52,4 +51,28 @@ function donateOffline() {
 			//donateCallback.success(data.data);
 		}
 	});
+}
+
+// Display event info
+if (location.href.indexOf("donate_applepay") > 0 || location.href.indexOf("donate_venmo") > 0 || location.href.indexOf("donate_amazon") > 0 || location.href.indexOf("donate_googlepay") > 0) {
+  let eid = jQuery('input[name=fr_id]').val();
+  let dtype = (jQuery('input[name=proxy_type_value]').val() == 20 || jQuery('input[name=proxy_type_value]').val() == 2) ? "p" : ((jQuery('input[name=proxy_type_value]').val() == 21) ? "e" : "t");
+  let pid = (dtype == "p") ? jQuery('input[name=cons_id]').val() : "";
+  let tid = (dtype == "t") ? jQuery('input[name=team_id]').val() : "";
+  let isDev = (jQuery('input[name=instance]').val() == "heartdev");
+  let tr_info = isDev ? "https://secure3.convio.net/heartdev/site/SPageNavigator/reus_donate_amazon_tr_info.html" : "https://www2.heart.org/site/SPageNavigator/reus_donate_amazon_tr_info.html";
+
+  jQuery.getJSON(tr_info+"?pgwrap=n&fr_id="+eid+"&team_id="+tid+"&cons_id="+pid+"&callback=?",function(data2){
+  if (data2.team_name != "" && dtype == "t") {
+    jQuery('.donation-form-container').before('<div class="donation-detail"><strong>Donating to Team Name:</strong><br/><a href="'+jQuery('input[name=from_url]').val()+'">'+data2.team_name+'</a></div>');
+  }
+  if (data2.event_title != " " && dtype == "e") {
+    jQuery('.donation-form-container').before('<div class="donation-detail"><strong>Donating to Event:</strong><br/><a href="'+jQuery('input[name=from_url]').val()+'">'+data2.event_title+'</a></div>');
+  }
+  if (data2.part_name != " " && dtype == "p") {
+    jQuery('.donation-form-container').before('<div class="donation-detail"><strong>Donating to Student:</strong><br/><a href="'+jQuery('input[name=from_url]').val()+'">'+data2.part_name+'</a></div>');
+  }
+
+  jQuery('input[name=form_id]').val(data2.don_form_id);
+  });
 }
