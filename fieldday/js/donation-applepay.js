eb06a597ba8f919bf7a9fbf9299ddb70733d3351
Stuart Sims <stuart.sims@heart.org>
2020-07-16 10:58:26 -0500

    Update form styling
    * Remove star for tos label checkbox
    * change details url for applepay

--- fieldday/js/donation-applepay.js
+++ fieldday/js/donation-applepay.js
@@ -16,19 +16,23 @@
 jQuery(document).ready(function() {
 	jQuery('#from_url_js').val(document.referrer);
 	    
-        var evid = jQuery.getQuerystring("FR_ID");
-	jQuery.getJSON('https://hearttools.heart.org/aha_2014/getEventDetail.php?event_id='+evid+'&callback=?',function(data){
-		if(data.eventdata != null) {
-                   var regtst = /\w{3}-+/;
-	   	   var match = regtst.exec(data.eventdata.greetingurl);
-                   if (match != null) {
-   		      jQuery('input[name=affiliate]').val(match[0].substr(0,3));
-                   } else {
-   		      jQuery('input[name=affiliate]').val('GEN');
-                   }
-                } else {
-		   jQuery('input[name=affiliate]').val('GEN');
-                }
+	var evid = jQuery.getQuerystring("FR_ID");
+	var apiURL = 'https://www2.heart.org/site/CRTeamraiserAPI?luminateExtend=1.7.1&method=getTeamraisersByInfo&name=%25%25%25&list_filter_column=frc.fr_id&list_filter_text='+evid+'&list_page_size=500&list_ascending=false&list_sort_column=event_date&api_key=wDB09SQODRpVIOvX&response_format=json&suppress_response_codes=true&v=1.0&ts=1536362358137';
+	if (jQuery('input[name=instance]').val() == "heartdev") {
+		apiURL = 'https://dev2.heart.org/site/CRTeamraiserAPI?luminateExtend=1.7.1&method=getTeamraisersByInfo&name=%25%25%25&list_filter_column=frc.fr_id&list_filter_text='+evid+'&list_page_size=500&list_ascending=false&list_sort_column=event_date&api_key=wDB09SQODRpVIOvX&response_format=json&suppress_response_codes=true&v=1.0&ts=1536362358137';
+	}
+	jQuery.getJSON(apiURL,function(data){
+		if(data.getTeamraisersResponse != null) {
+			var regtst = /\w{3}-+/;
+					var match = regtst.exec(data.getTeamraisersResponse.teamraiser.greeting_url);
+			if (match != null) {
+				jQuery('input[name=affiliate]').val(match[0].substr(0,3));
+			} else {
+				jQuery('input[name=affiliate]').val('GEN');
+			}
+		} else {
+			jQuery('input[name=affiliate]').val('GEN');
+		}
 	});
 	    
 	/* UI handlers for the donation form example */
