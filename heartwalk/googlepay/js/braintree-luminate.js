b58df6d5d4cf7187cd6a39eaf2bcc5305b2f9f56
Stuart Sims <stuart.sims@heart.org>
2020-08-14 14:28:48 -0500

    WIP Update URL for hearttools upgrade

--- heartwalk/googlepay/js/braintree-luminate.js
+++ heartwalk/googlepay/js/braintree-luminate.js
@@ -176,7 +176,7 @@ var braintree_aha = {
 				
 		var tokenURL = "https://hearttools.heart.org/braintree_new/checkout-tr.php";
 		if (jQuery('input[name=instance]').val() == "heartdev") {
-			tokenURL = "https://hearttools.heart.org/braintree_new/checkout-tr-test.php";
+			tokenURL = "https://tools.heart.org/braintree_new/checkout-tr-test.php";
 		}
 		jQuery.getJSON(tokenURL + '?callback=?', postParams)
 			.done(function(data) {
