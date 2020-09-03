285d36fb3d3ad349cafee227e6cf07bb25ac7253
Stuart Sims <stuart.sims@heart.org>
2020-09-03 11:34:52 -0500

    HW add double donation functions

--- heartwalk/js/donation.js
+++ heartwalk/js/donation.js
@@ -44,8 +44,8 @@ jQuery('#cover_fee, .radio-level').on('click', function(){
 
 
 // Post offline gift to Luminate
-function donateOffline() {
-	var params = jQuery('.donation-form').serialize();
+function donateOffline(donateOfflineCallback) {
+	let params = jQuery('.donation-form').serialize();
 
 	jQuery.ajax({
 		method: "POST",
@@ -53,10 +53,69 @@ function donateOffline() {
 		cache: false,
 		dataType: "json",
 		url: "https://tools.heart.org/donate/convio-offline/addOfflineDonation-tr.php?" + params + "&callback=?",
-		success: function(data) {
-			//donateCallback.success(data.data);
-		}
+		success: donateOfflineCallback
 	});
+}
+
+/**
+ * Get the Transaction ID and Confirmation Code for transactions added via the API
+ * @param {*} responseData - From the donateOffline success callback
+ */
+function donateOfflineCallback(responseData) {
+	const nameField = $('input[name=campaign_name]').length ? $('input[name=campaign_name]').val() : "American Heart Association";
+	const campaign_name = ($('input[name=instance]').val() == "heartdev" ? "heartdev " : "") + nameField;
+
+	const widgetData = {
+		transactionId: responseData.data.donationResponse.donation.transaction_id,
+		confirmationCode: responseData.data.donationResponse.donation.confirmation_code,
+		transactionDate: responseData.data.donationResponse.donation.date_time,
+		email: $('input[name="donor.email"]').val(),
+		firstName: $('input[name="donor.name.first"]').val(),
+		lastName: $('input[name="donor.name.last"]').val(),
+		amt: $('input[name=other_amount]').val(),
+		form: campaign_name
+	};
+
+	if (jQuery('input[name=doublethedonation_company_id]').length && jQuery('input[name=doublethedonation_company_id]').val() !== "") {
+		doubleDonationConfirmation(widgetData);
+	}
+}
+
+/**
+ * Post matching gift info
+ * @param {*} widgetData
+ */
+function doubleDonationConfirmation(widgetData) {
+	const ddCompanyId = jQuery('input[name=doublethedonation_company_id]').val();
+
+	var domain = doublethedonation.integrations.core.strip_domain(widgetData.email);
+	doublethedonation.plugin.load_config();
+	doublethedonation.plugin.set_donation_id(widgetData.confirmationCode);
+	doublethedonation.plugin.set_donation_campaign(widgetData.form);
+	doublethedonation.plugin.email_domain(domain);
+
+	if (ddCompanyId !== "") {
+		doublethedonation.plugin.set_company(ddCompanyId);
+	}
+
+	doublethedonation.integrations.core.register_donation({
+		"360matchpro_public_key": "w5JH5j9ID4Cf6zMh",
+		"campaign": widgetData.form,
+		"donation_identifier": widgetData.confirmationCode,
+		"donation_amount": widgetData.amt,
+		"donor_first_name": widgetData.firstName,
+		"donor_last_name": widgetData.lastName,
+		"donor_email": widgetData.email,
+		"doublethedonation_company_id": ddCompanyId,
+		"doublethedonation_status": null
+	});
+
+	// delay triggering the widget
+	setTimeout(function() {
+		if (window.doublethedonation) {
+			doublethedonation.plugin.load_plugin();
+		}
+	}, 1000);
 }
 
 // Display event info
