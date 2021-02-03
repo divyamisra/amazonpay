6ed9b2190541dad59c88b817acfb1204c4023425
Stuart S <31454574+StuHeart@users.noreply.github.com>
2021-02-03 12:15:30 -0600

    Add focus to top of form
    Move focus to top of form when errors are presented.

--- fieldday/js/donation-googlepay.js
+++ fieldday/js/donation-googlepay.js
@@ -114,7 +114,10 @@ jQuery(document).ready(function() {
                     }
 					braintree_aha.submitGooglePayDonation();
                 } else {
-					$('label.error').attr('role','alert').attr('aria-atomic','true');
+			$('label.error').attr('role','alert').attr('aria-atomic','true');
+			const h = document.querySelector("h2");
+			h.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+			h.focus();
                     return false;
                 }
             });
