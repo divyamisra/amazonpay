10f359b02d46c840a05cf90c48034f247858680e
Stuart Sims <stuart.sims@heart.org>
2021-02-19 14:23:09 -0600

    Update Amazon URI decoding

--- ym-primary/js/amazon-pay-v2.js
+++ ym-primary/js/amazon-pay-v2.js
@@ -139,7 +139,7 @@ function populateForm(lsForm) {
 	}
 	// populate inputs
 	jQuery('.donation-form input').not('input:checkbox, input:radio').each(function(){
-		jQuery(this).val(decodeURI(donateData[this.name]).replace('%40', '@').replaceAll('+', ' ').replaceAll('%2B', ' ').replaceAll('%3A', ':').replaceAll('%2C', ',').replaceAll('%26', '&'));
+		jQuery(this).val(decodeURIComponent(donateData[this.name]).replaceAll('+', ' '));
 	});
 	// populate selects
 	jQuery('.donation-form select').each(function(){
